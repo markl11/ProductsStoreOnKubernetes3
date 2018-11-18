6cf5223a858cef16655bd5473af762d3f2723a72
Mark <mark@mjlnet.com>
2018-11-18 12:52:48 +0000

    first commit

---
+++ MvcApp3/Data/ProductsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcApp3.Models
+{
+    public class ProductsContext : DbContext
+    {
+        public ProductsContext (DbContextOptions<ProductsContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<MvcApp3.Models.Product> Product { get; set; }
+    }
+}
